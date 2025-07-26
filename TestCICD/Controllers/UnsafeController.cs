6ef883ae60221912e072363c3a85febd5d8c13a1
DEFC Utilities <110030816+AminaElsheikh@users.noreply.github.com>
2025-07-26 18:00:37 +0300

    00

--- TestCICD/Controllers/UnsafeController.cs
+++ TestCICD/Controllers/UnsafeController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
@@ -9,15 +9,20 @@ namespace TestCICD.Controllers
     public class UnsafeController : ControllerBase
     {
         [HttpPost("deserialize")]
-        public IActionResult Deserialize([FromBody] byte[] input)
+        public IActionResult Deserialize([FromBody] SafeInput model)
         {
-            // ❌ Insecure Deserialization (detected by CodeQL)
+            
             BinaryFormatter formatter = new BinaryFormatter();
-            using (MemoryStream stream = new MemoryStream(input))
+            using (MemoryStream stream = new MemoryStream(model.input))
             {
                 object obj = formatter.Deserialize(stream); // Vulnerability here
                 return Ok(obj.ToString());
             }
         }
+    }
+    public class SafeInput
+    {
+        public string Data { get; set; }
+        public byte[] input { get; set; }
     }
 }
